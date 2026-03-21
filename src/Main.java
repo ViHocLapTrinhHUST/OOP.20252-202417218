cf39bade999c3caef112ebfdae509fc6e4a3f1e7
Nguyen Phan Vi <vi.np2417218@sis.hust.edu.vn>
2026-03-21 08:55:55 +0700

    init: Initialize HW04 folder and BankAccount class properties

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+package HW04;
+
+class BankAccount {
+    private String name;
+    private double balance;
+}
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
