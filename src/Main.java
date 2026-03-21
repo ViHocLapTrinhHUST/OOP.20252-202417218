17c69ebc3c212f1e2e1b8704b944bdacc1736299
Nguyen Phan Vi <vi.np2417218@sis.hust.edu.vn>
2026-03-21 09:00:56 +0700

    feat: Add Constructors and Getters/Setters

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,19 @@ package HW04;
 class BankAccount {
     private String name;
     private double balance;
+
+    public BankAccount(String name, double balance) {
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
 }
 
 public class Main {
